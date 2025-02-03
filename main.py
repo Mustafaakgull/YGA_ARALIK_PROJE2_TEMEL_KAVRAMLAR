14a7ad53f3caaddaf23fc3b0f0cd5665eee5d66d
Mustafaakgull <mustafaakgul1@stu.aydin.edu.tr>
2025-02-03 16:59:55 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+num1 = float(input("Birinci sayıyı girin: "))
+num2 = float(input("İkinci sayıyı girin: "))
+operation = input("İşlem seçin (toplama, çıkarma, çarpma, bölme): ").lower()
+
+if operation == "toplama":
+    print(f"Sonuç: {num1 + num2}")
+elif operation == "çıkarma":
+    print(f"Sonuç: {num1 - num2}")
+elif operation == "çarpma":
+    print(f"Sonuç: {num1 * num2}")
+elif operation == "bölme":
+    if num2 != 0:
+        print(f"Sonuç: {num1 / num2}")
+    else:
+        print("Bir sayıyı sıfıra bölemezsiniz!")
+else:
+    print("Geçersiz işlem.")
