5338398bacedb57182e2dd2ca42e973a99accad2
Mustafaakgull <mustafaakgul1@stu.aydin.edu.tr>
2025-02-03 17:21:58 +0300

    sonuclar.txt added, results will be added to this txt file.

--- main.py
+++ main.py
@@ -2,16 +2,24 @@ num1 = float(input("Birinci sayıyı girin: "))
 num2 = float(input("İkinci sayıyı girin: "))
 operation = input("İşlem seçin (toplama, çıkarma, çarpma, bölme): ").lower()
 
+result = None
+
 if operation == "toplama":
-    print(f"Sonuç: {num1 + num2}")
+    result = num1 + num2
 elif operation == "çıkarma":
-    print(f"Sonuç: {num1 - num2}")
+    result = num1 - num2
 elif operation == "çarpma":
-    print(f"Sonuç: {num1 * num2}")
+    result = num1 * num2
 elif operation == "bölme":
     if num2 != 0:
-        print(f"Sonuç: {num1 / num2}")
+        result = num1 / num2
     else:
-        print("sayı sıfıra bölünemez...")
+        print("Bir sayıyı sıfıra bölemezsiniz!")
 else:
     print("Geçersiz işlem.")
+
+if result is not None:
+    print(f"Sonuç: {result}")
+
+with open("sonuclar.txt", "a") as file:
+    file.write(f"{num1} {operation} {num2} = {result}\n")
